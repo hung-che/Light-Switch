c13fae9ef326c56d4c72ca631560c7ae9295ff2d
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2020-08-25 14:03:03 -0500

    Update switch.ino

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,8 +1,14 @@
+/* Code written by Hung Che.
+ * This is the code for my "Touch less, Do more" challenge submission
+ */
+ 
 #include <Servo.h> //include servo library
 
 //declare servo objects
 Servo on;
 Servo off;
+
+int light_switch = 0; //light status: 0 = on, 1 = off
 
 void setup() {
   Serial.begin(9600); //begin serial for debugging
@@ -10,12 +16,31 @@ void setup() {
   off.attach(8); //attach the "off" servo to pin D8
   pinMode(7, INPUT); //declare the sound sensor pin to INPUT
 
+  //set the initial positions of the servos
   on.write(180);
-  off.write(0);
-  delay(1000);
-  off.write(30); 
+  off.write(0); 
 }
 
 void loop() {
-
+  int sensor = digitalRead(7); //obtain data from the sensor port
+  if(sensor == 1){ // if sound is detected ...
+    
+    if(light_switch == 0){ // ...and if the light is on...
+      
+      Serial.println("off"); //print "off" the the serial monitor
+      on.write(180);
+      delay(5);
+      off.write(30);
+      delay(2000);
+      light_switch ++; //change status to "off"
+      
+    }else if(light_switch==1){ // ... and if the light is off...
+       Serial.println("on");
+       off.write(0);
+       delay(5);
+       on.write(150);
+       delay(2000);
+       light_switch = 0; //change status to "on"
+    }
+  }
 }
